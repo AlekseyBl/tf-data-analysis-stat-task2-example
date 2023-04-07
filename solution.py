ab7fb03b14ba94169ff3ca5ccc625479c8a274ce
AlekseyBl <130070481+AlekseyBl@users.noreply.github.com>
2023-04-07 21:01:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,13 +6,8 @@ from scipy.stats import expon
 
 chat_id = 6130040059 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    t = 59
-    loc = (2/t**2)*(x.mean()-0.5)
-    scale = (2/t**2)*(np.sqrt(np.var(x))+1) / np.sqrt(len(x))
-    return loc - scale * expon.ppf(1 - alpha / 2), \
-           loc - scale * expon.ppf(alpha / 2)
+
+    def solution(p: float, x: np.array) -> tuple:
+    mean1 = 2 / 59 ** 2 * (gamma.ppf((1 - p) / 2, x.size) / x.size + x.mean() - 1 / 2)
+    mean2 = 2 / 59 ** 2 * (gamma.ppf((1 + p) / 2, x.size) / x.size + x.mean() - 1 / 2)
+    return mean1, mean2
