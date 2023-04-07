0cb3f9c4b9d9b56dca2f0df5b2f1fc6d2e31d99e
AlekseyBl <130070481+AlekseyBl@users.noreply.github.com>
2023-04-07 21:25:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import expon
+from scipy.stats import gamma
 
 
 chat_id = 6130040059 # Ваш chat ID, не меняйте название переменной
 
-
-    def solution(p: float, x: np.array) -> tuple:
+def solution(p: float, x: np.array) -> tuple:
     mean1 = 2 / 59 ** 2 * (gamma.ppf((1 - p) / 2, x.size) / x.size + x.mean() - 1 / 2)
     mean2 = 2 / 59 ** 2 * (gamma.ppf((1 + p) / 2, x.size) / x.size + x.mean() - 1 / 2)
     return mean1, mean2
